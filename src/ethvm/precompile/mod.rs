84f01483d2a1fcb26e685fc14ec2c4c19ad78e04
ccmlm <fanhui.x@gmail.com>
2022-01-28 11:41:24 +0800

    add more precompiles

--- src/ethvm/precompile/mod.rs
+++ src/ethvm/precompile/mod.rs
@@ -4,6 +4,10 @@
 
 use evm::executor::stack::PrecompileFn;
 use fevm::Precompile;
+use fevm_precompile_blake2::Blake2F;
+use fevm_precompile_bn128::{Bn128Add, Bn128Mul, Bn128Pairing};
+use fevm_precompile_curve25519::{Curve25519Add, Curve25519ScalarMul};
+use fevm_precompile_ed25519::Ed25519Verify;
 use fevm_precompile_modexp::Modexp;
 use fevm_precompile_sha3fips::Sha3FIPS256;
 use fevm_precompile_simple::{
@@ -23,6 +27,13 @@ pub(crate) static PRECOMPILE_SET: Lazy<BTreeMap<H160, PrecompileFn>> = Lazy::new
         idx_to_h160(5) => Modexp::execute,
         idx_to_h160(6) => ECRecoverPublicKey::execute, // Compitable with Findora
         idx_to_h160(7) => Sha3FIPS256::execute, // Compitable with Findora
+        idx_to_h160(1024) => Blake2F::execute,
+        idx_to_h160(1025) => Bn128Pairing::execute,
+        idx_to_h160(1026) => Bn128Add::execute,
+        idx_to_h160(1027) => Bn128Mul::execute,
+        idx_to_h160(1028) => Curve25519Add::execute,
+        idx_to_h160(1029) => Curve25519ScalarMul::execute,
+        idx_to_h160(1030) => Ed25519Verify::execute,
     }
 });
 
